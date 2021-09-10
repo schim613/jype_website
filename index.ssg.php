cef520bdc49073cf20fa55648f8728608e11c5bc
schim613 <uximin@naver.com>
2021-09-10 18:19:54 +0900

    함 봤어요

--- index.ssg.php
+++ index.ssg.php
@@ -1,3 +1,11 @@
 <?php require_once "head.php"; ?>
-
+<div class="main-banner">
+  <!-- <iframe width="1920" height="800" src="https://www.youtube.com/embed/EaswWiwMVs8?autoplay=1&mute=1?" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+  <ul class="video">
+    <li><a href="#"><video src="resource/img/StrayKids_소리꾼_MV.mp4" autoplay loop muted></video></a></li>
+    <li><a href="#"><video src="resource/img/" autoplay loop muted></video></a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+  </ul>
+</div>
 <?php require_once "foot.php"; ?>
