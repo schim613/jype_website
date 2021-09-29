8a9d25433e2a5f9fa436b2edce548766a1dd1cb7
schim613 <uximin@naver.com>
2021-09-29 17:53:26 +0900

    서브페이지-아티스트 수정

--- artist_skz.ssg.php
+++ artist_skz.ssg.php
@@ -11,30 +11,103 @@
     </div>
     <div class="artist_content">
       <div class="con-1">
-        <div>VIDEO</div>
-        <iframe width="1140" height="560" src="https://www.youtube.com/embed/EaswWiwMVs8" title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen></iframe>
+        <div>PROFILE</div>
+        <ul class="member-list flex">
+          <li>
+            <a href="#">
+              <img src="resource/img/profile_bangchan.png" alt="">
+              <span>BANG CHAN</span>
+              <span>방찬</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="resource/img/profile_leeknow.png" alt="">
+              <span>lee know</span>
+              <span>리노</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="resource/img/profile_changbin.png" alt="">
+              <span>changbin</span>
+              <span>창빈</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="resource/img/profile_hyunjin.png" alt="">
+              <span>hyunjin</span>
+              <span>현진</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="resource/img/profile_han.png" alt="">
+              <span>han</span>
+              <span>한</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="resource/img/profile_felix.png" alt="">
+              <span>felix</span>
+              <span>필릭스</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="resource/img/profile_seungmin.png" alt="">
+              <span>seungmin</span>
+              <span>승민</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="resource/img/profile_in.png" alt="">
+              <span>i.n</span>
+              <span>아이엔</span>
+            </a>
+          </li>
+        </ul>
       </div>
       <div class="con-2">
+        <div>VIDEO</div>
+        <div class="video-list flex">
+          <iframe class="mr-5" width="840" height="460" src="https://www.youtube.com/embed/EaswWiwMVs8"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+          <div>
+            <iframe class="mb-5" width="420" height="220" src="https://www.youtube.com/embed/M7lIml8-eHU"
+              title="YouTube video player" frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen></iframe>
+            <iframe width="420" height="220" src="https://www.youtube.com/embed/X-uJtV8ScYk"
+              title="YouTube video player" frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+      <div class="con-3">
         <div>RELEASES</div>
-        <ul class="release flex">
+        <ul class="release-list flex">
           <li><a href="#">
-            <img src="resource/img/album-1.png" alt="">
-          </a></li>
+              <img src="resource/img/album-1.png" alt="">
+            </a></li>
           <li><a href="#">
-            <img src="resource/img/album-2.png" alt="">
-          </a></li>
+              <img src="resource/img/album-2.png" alt="">
+            </a></li>
           <li><a href="#">
-            <img src="resource/img/album-3.png" alt="">
-          </a></li>
+              <img src="resource/img/album-3.png" alt="">
+            </a></li>
           <li><a href="#">
-            <img src="resource/img/album-4.png" alt="">
-          </a></li>
+              <img src="resource/img/album-4.png" alt="">
+            </a></li>
           <li><a href="#">
-            <img src="resource/img/album-5.png" alt="">
-          </a></li>
+              <img src="resource/img/album-5.png" alt="">
+            </a></li>
         </ul>
       </div>
     </div>
